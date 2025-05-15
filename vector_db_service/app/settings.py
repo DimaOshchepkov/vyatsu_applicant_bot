64c16b6a208970982c401dcae5c78340ee2811d1
DimaOshcpepkov <odo.2003@yandex.ru>
2025-05-15 13:44:20 +0300

    Ci настроен

--- vector_db_service/app/settings.py
+++ vector_db_service/app/settings.py
@@ -15,4 +15,27 @@ class QdrantSettings(BaseSettings):
         extra="ignore"  
     )
 
+
 qdrant_settings = QdrantSettings()
+
+
+class DBSettings(BaseSettings):
+    db_host: str = Field(default="", description="Database host")
+    db_port: int = Field(default=-1, description="Database port")
+    db_name: str = Field(default="", description="Database name")
+    db_user: str = Field(default="", description="Database user")
+    db_pass: str = Field(default="", description="Database password")
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"  
+    )
+
+    def get_connection_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.db_user}:{self.db_pass}"
+            f"@{self.db_host}:{self.db_port}/{self.db_name}"
+        )
+        
+        
+db_settings = DBSettings()
