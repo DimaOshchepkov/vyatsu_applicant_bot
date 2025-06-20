e7c2ed5cb8e71b4d9562a86b284eada5fc0bdc5a
DimaOshchepkov <odo.2003@yandex.ru>
2025-06-20 20:49:23 +0300

    Исправил тесты

--- src/tests/infrastructure/repositories/test_base_repository.py
+++ src/tests/infrastructure/repositories/test_base_repository.py
@@ -2,32 +2,32 @@ import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from shared.models import Category
-from tactic.domain.entities.category import CategoryDomain
+from tactic.domain.entities.category import CategoryDomain, CreateCategoryDomain
 from tactic.infrastructure.repositories.base_repository import BaseRepository
 
 
 @pytest.mark.asyncio
 async def test_add_and_get(session_with_drop_after: AsyncSession):
-    repo = BaseRepository[CategoryDomain, Category](
-        session_with_drop_after, CategoryDomain, Category
+    repo = BaseRepository[CategoryDomain, Category, CreateCategoryDomain](
+        session_with_drop_after, CategoryDomain, Category, CreateCategoryDomain
     )
-    category_domain = CategoryDomain(id=1, title="Alice", parent_id=None)
+    category_domain = CreateCategoryDomain(title="Alice", parent_id=None)
 
     added = await repo.add(category_domain)
-    assert added == category_domain
+    assert added.title == category_domain.title
 
     fetched = await repo.get(1)
-    assert fetched == category_domain
+    assert fetched.title if fetched else '' == category_domain.title
 
 
 @pytest.mark.asyncio
 async def test_get_all(session_with_drop_after: AsyncSession):
-    repo = BaseRepository[CategoryDomain, Category](
-        session_with_drop_after, CategoryDomain, Category
+    repo = BaseRepository[CategoryDomain, Category, CreateCategoryDomain](
+        session_with_drop_after, CategoryDomain, Category, CreateCategoryDomain
     )
-    await repo.add(CategoryDomain(id=1, title="A", parent_id=None))
-    await repo.add(CategoryDomain(id=2, title="B", parent_id=None))
+    await repo.add(CreateCategoryDomain(title="A", parent_id=None))
+    await repo.add(CreateCategoryDomain(title="B", parent_id=None))
 
     all_categories = await repo.get_all()
     assert len(all_categories) == 2
-    assert all(category.id in [1, 2] for category in all_categories)
+    assert all(category.title in ["A", "B"] for category in all_categories)
--- src/tests/infrastructure/services/telegram_message_sender/test_telegram_message_sender.py
+++ src/tests/infrastructure/services/telegram_message_sender/test_telegram_message_sender.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 from typing import Annotated
 from unittest.mock import AsyncMock
 
@@ -9,7 +9,7 @@ from arq.jobs import Job
 from pytest_mock import MockerFixture
 
 import tactic.infrastructure.telegram.telegram_message_sender as telegram_module
-from tactic.domain.entities.timeline_event import TimelineEventDTO
+from tactic.domain.entities.timeline_event import SendEvent, TimelineEventDTO
 from tactic.infrastructure.telegram.telegram_message_sender import (
     TelegramMessageSender,  # Для клиента arq
 )
@@ -17,7 +17,7 @@ from tactic.infrastructure.telegram.telegram_message_sender import (
 
 @pytest.fixture
 def mock_bot(mocker: MockerFixture) -> Annotated[Bot, AsyncMock]:
-    return mocker.Mock(spec=Bot)  # Подразумеваем, что используем aiogram.Bot
+    return mocker.AsyncMock(spec=Bot)  # Подразумеваем, что используем aiogram.Bot
 
 
 @pytest.fixture
@@ -28,17 +28,16 @@ def mock_redis(mocker: MockerFixture) -> Annotated[ArqRedis, AsyncMock]:
 
 
 @pytest.fixture
-def sender(mock_bot, mock_redis):
-    return TelegramMessageSender(mock_bot, mock_redis)
+def sender(mock_redis):
+    return TelegramMessageSender(mock_redis)
 
 
 @pytest.fixture
 def fake_event():
-    return TimelineEventDTO(
+    return SendEvent(
         id=1,
-        name_id=-1,
-        deadline=datetime.today() + timedelta(days=2),
-        event_name="Test Event",
+        when=datetime.now() + timedelta(days=2),
+        message="Test Event",
     )
 
 
@@ -51,16 +50,16 @@ def mock_job(mocker: MockerFixture) -> Annotated[Job, AsyncMock]:
 
 
 @pytest.mark.asyncio
-async def test_schedule_message_future(sender, fake_event):
+async def test_schedule_message_future(sender, fake_event, mock_redis):
     # Сдвигаем дату далеко в будущее, чтобы delay точно был положительным
-    fake_event.deadline = date.today() + timedelta(days=5)
+    fake_event.when = datetime.now() + timedelta(days=5)
 
     await sender.schedule_message(chat_id=123, event=fake_event)
 
-    sender.redis.enqueue_job.assert_awaited_once()
-    args, kwargs = sender.redis.enqueue_job.call_args
+    mock_redis.enqueue_job.assert_awaited_once()
+    args, kwargs = mock_redis.enqueue_job.call_args
     assert kwargs["chat_id"] == 123
-    assert kwargs["text"] == fake_event.event_name
+    assert kwargs["text"] == fake_event.message
     assert "_defer_by" in kwargs
     assert "_job_id" in kwargs
     assert kwargs["_defer_by"] > 0
@@ -69,13 +68,12 @@ async def test_schedule_message_future(sender, fake_event):
 # Тест планирования сообщения в прошлом
 @pytest.mark.asyncio
 async def test_schedule_message_past(
-    sender: TelegramMessageSender, fake_event, mocker: MockerFixture
+    sender: TelegramMessageSender, fake_event, mock_redis, mocker: MockerFixture
 ):
-    fake_event.deadline = date.today() - timedelta(days=1)  # deadline в прошлом
+    fake_event.when = datetime.now() - timedelta(days=1)  # deadline в прошлом
     await sender.schedule_message(chat_id=123, event=fake_event)
 
-    sender.bot.send_message.assert_called_once_with(123, fake_event.event_name)
-    sender.redis.enqueue_job.assert_not_called()
+    mock_redis.enqueue_job.assert_not_called()
 
 
 @pytest.mark.asyncio
@@ -88,7 +86,7 @@ async def test_cancel_scheduled_message_found(
     mock_job.status.return_value = "queued"
     mocker.patch.object(telegram_module, "Job", return_value=mock_job)
 
-    await sender.cancel_scheduled_message(chat_id=123, event=fake_event)
+    await sender.cancel_scheduled_message(chat_id=123, event_id=fake_event.id)
 
     mock_job.abort.assert_awaited_once()
 
@@ -104,30 +102,9 @@ async def test_cancel_scheduled_message_not_found(
     mock_job.status.return_value = "not_found"
     mocker.patch.object(telegram_module, "Job", return_value=mock_job)
 
-    await sender.cancel_scheduled_message(chat_id=123, event=fake_event)
+    await sender.cancel_scheduled_message(chat_id=123, event_id=fake_event.id)
 
     mock_job.abort.assert_not_called()
 
 
-@pytest.mark.asyncio
-async def test_schedule_messages_bulk(
-    sender: TelegramMessageSender,
-    fake_event: TimelineEventDTO,
-    mocker: MockerFixture,
-):
-    mock_schedule = mocker.patch.object(
-        sender, "schedule_message", new=mocker.AsyncMock()
-    )
-    await sender.schedule_messages_bulk(chat_id=123, events=[fake_event] * 3)
-    assert mock_schedule.await_count == 3
 
-
-@pytest.mark.asyncio
-async def test_cancel_scheduled_messages_bulk(
-    sender: TelegramMessageSender, fake_event, mocker: MockerFixture
-):
-    mock_cancel = mocker.patch.object(
-        sender, "cancel_scheduled_message", new=mocker.AsyncMock()
-    )
-    await sender.cancel_scheduled_messages_bulk(chat_id=123, events=[fake_event] * 2)
-    assert mock_cancel.await_count == 2
